039367b5de164cdc05213b326e83e0f3ce3069ed
eric_cho <eric3cho@gmail.com>
2024-12-03 15:12:54 -0800

    day3 setting up

--- d2.py
+++ d2.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-# 439
+
 # weight each left id by its occurrence and sum
+#
 def rednosed_reports():
     file = open('day2.txt')
     safe = 0
     maybe = []
     for line in file:
         levels = [int(num) for num in line.split()]
-        # first pass for safe, add questionable to maybe
+        # first pass for safe, add questionable sequences to maybe
         if valid2(levels) and valid1(levels): safe += 1
         else: maybe.append(levels)
-    # second pass of maybe,
+    # second pass of maybe
     for test in maybe:
         valid = False
         for x in range(len(test)):
@@ -28,6 +29,6 @@ def valid1(nums):
         if abs(nums[x] - nums[x + 1]) > 3 or abs(nums[x] - nums[x + 1]) < 1: return False
     return True
 
-
+# check if all ascending or all descending
 def valid2(nums):
     return nums == sorted(nums) or nums == sorted(nums, reverse=True)
