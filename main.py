33944dfc58a900ec5b9708fcb585531d3f92c3d0
eric_cho <eric3cho@gmail.com>
2024-12-02 16:40:32 -0800

    format

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-file = open('day1.txt', 'r')
-left = []
-right = []
-for line in file:
-    clean = line.split()
-    left.append(int(clean[0]))
-    right.append(int(clean[1]))
-left = np.sort(np.array(left))
-right = np.sort(np.array(right))
-total = 0
-for x in range(len(left)):
-    total += np.abs(left[x] - right[x])
-print(total)
+
+# day 1
+def abs_diff():
+    file = open('day1.txt')
+    left = []
+    right = []
+    for line in file:
+        clean = line.split()  # creates array of strings
+        left.append(int(clean[0]))  # cast as int, add to list
+        right.append(int(clean[1]))
+    left = np.sort(left)
+    right = np.sort(right)
+    print(np.sum(np.abs(left - right)))
+
+
+abs_diff()
+
