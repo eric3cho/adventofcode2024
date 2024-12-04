eb6ca75f4fd69f0683a5c621326f595d13c8e068
eric_cho <eric3cho@gmail.com>
2024-12-04 00:25:59 -0800

    trying finditer

--- d3.py
+++ d3.py
@@ -5,12 +5,13 @@ def mull_it_over():
     file = open('day3.txt')
     total = 0
     store = []
-    # search every line for matches to 'mul'
-    # slice max possible segment and add to store
+    # search every line for matches to 'mul', slice by max possible size and add to store
     for line in file:
         # get start of all matches in line
         indices = [match.start() for match in re.finditer('mul', line)]
         for i in indices:
             store.append(line[i+3:i+12])
-    for x in store:
-        for
+    # go through store and multiply out
+    for match in store:
+        # replace ( and ) with * and split on *
+        # edge cases: * inside ()
