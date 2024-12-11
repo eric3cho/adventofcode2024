31f04dd93531928cfedc14516d50a570909059d2
eric_cho <eric3cho@gmail.com>
2024-12-11 14:19:36 -0800

    regex pattern fixed

--- d3.py
+++ d3.py
@@ -4,18 +4,21 @@ import re
 def mull_it_over():
     file = open('day3.txt')
     total = 0
+    store = []
     do = True
-    # search every line for matches to 'mul', slice by max possible size and add to store
-    seq = 'mul\(\d{1,3},\d{1,3}\)'
-    store = re.finditer(r'seq|do|don\'t', file)
-    # go through store checking for ( , )
-    for match in store:
-        start = match.find('(')
-        end = match.find(')')
-        if start < end and ',' in match[start:end]:
-            vals = match[start+1:end].split(',')
-            a = vals[0]
-            b = vals[1]
-            if a.isdigit() and b.isdigit():
-                total += int(a)*int(b)
-    print(total)
+    store = [re.findall(r'mul\(\d{1,3},\d{1,3}\)|do|don\'t', line) for line in file]
+    print(store)
+
+
+
+    # # go through store checking for ( , )
+    # for match in store:
+    #     start = match.find('(')
+    #     end = match.find(')')
+    #     if start < end and ',' in match[start:end]:
+    #         vals = match[start+1:end].split(',')
+    #         a = vals[0]
+    #         b = vals[1]
+    #         if a.isdigit() and b.isdigit():
+    #             total += int(a)*int(b)
+    # print(total)
