d0c69d96feee2e921d2cab31748bb5a6af71317a
eric_cho <eric3cho@gmail.com>
2024-12-11 22:27:18 -0800

    diagonals set

--- d4.py
+++ d4.py
@@ -7,27 +7,32 @@ def ceres_search():
         for x in range(y):
             if grid[y][x] == 'X':
                 found += check(grid, y, x)
+
+
 # given grid and coords of X, check around
 def check(grid, y, x):
     count = 0
-    # right limit
+    # RIGHT
     if x < len(grid[0])-2:  # at least 7
-        # right
         if grid[y][x+1:x+4] == 'MAS': count+=1
+    # LEFT
     if x > 2:   # at least 3
-        # left
         if grid[y][x-3:x] == 'SAM': count+=1
+    # TOP
     if y > 2:
-        # top
         if grid[y-3:y][x] == 'SAM': count+=1
+        # DIAG UP RIGHT
+        if x < len(grid[0])-2:
+            if grid[y - 1][x + 1] == 'M' and grid[y - 2][x + 2] == 'A' and grid[y - 3][x + 3] == 'S': count += 1
+        # DIAG UP LEFT
+        if x > 2:
+            if grid[y - 1][x - 1] == 'M' and grid[y - 2][x - 2] == 'A' and grid[y - 3][x - 3] == 'S': count += 1
+    # DOWN
     if y < len(grid)-2:
-        # down
         if grid[y+1:y+4][x] == 'MAS': count+=1
-    # diag up right
-    if grid[y-1][x+1] == 'M' and grid[y-2][x+2] == 'A' and grid[y-3][x+3] == 'S': count+=1
-    # diag up left
-    if grid[y-1][x-1] == 'M' and grid[y-2][x-2] == 'A' and grid[y-3][x-3] == 'S': count += 1
-    # diag down right
-    if grid[y+1][x+1] == 'M' and grid[y-2][x+2] == 'A' and grid[y-3][x+3] == 'S': count += 1
-    # diag down left
-    if grid[y+1][x-1] == 'M' and grid[y+2][x-2] == 'A' and grid[y+3][x-3] == 'S': count += 1
+        # DIAG DOWN RIGHT
+        if x < len(grid[0]) - 2:
+            if grid[y + 1][x + 1] == 'M' and grid[y - 2][x + 2] == 'A' and grid[y - 3][x + 3] == 'S': count += 1
+        # DIAG DOWN LEFT
+        if x > 2:
+           if grid[y+1][x-1] == 'M' and grid[y+2][x-2] == 'A' and grid[y+3][x-3] == 'S': count += 1
