b73ea3ee4130e98d3025f1e1178b7c1b570b9c20
eric_cho <eric3cho@gmail.com>
2024-12-11 22:15:43 -0800

    right, left, top, down set

--- d4.py
+++ d4.py
@@ -11,15 +11,18 @@ def ceres_search():
 def check(grid, y, x):
     count = 0
     # right limit
-    if x < len(grid[0])-2:
-    # right
-    if grid[y][x+1:x+4] == 'MAS': count+=1
-    # left
-    if grid[y][x-3:x] == 'SAM': count+=1
-    # top
-    if grid[y-3:y][x] == 'SAM': count+=1
-    # down
-    if grid[y+1:y+4][x] == 'MAS': count+=1
+    if x < len(grid[0])-2:  # at least 7
+        # right
+        if grid[y][x+1:x+4] == 'MAS': count+=1
+    if x > 2:   # at least 3
+        # left
+        if grid[y][x-3:x] == 'SAM': count+=1
+    if y > 2:
+        # top
+        if grid[y-3:y][x] == 'SAM': count+=1
+    if y < len(grid)-2:
+        # down
+        if grid[y+1:y+4][x] == 'MAS': count+=1
     # diag up right
     if grid[y-1][x+1] == 'M' and grid[y-2][x+2] == 'A' and grid[y-3][x+3] == 'S': count+=1
     # diag up left
