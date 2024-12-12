fdbac11cf494856963ea65865d3856f8edd03b02
eric_cho <eric3cho@gmail.com>
2024-12-11 22:05:21 -0800

    check conditions done

--- d4.py
+++ d4.py
@@ -7,9 +7,11 @@ def ceres_search():
         for x in range(y):
             if grid[y][x] == 'X':
                 found += check(grid, y, x)
-
+# given grid and coords of X, check around
 def check(grid, y, x):
     count = 0
+    # right limit
+    if x < len(grid[0])-2:
     # right
     if grid[y][x+1:x+4] == 'MAS': count+=1
     # left
@@ -19,9 +21,10 @@ def check(grid, y, x):
     # down
     if grid[y+1:y+4][x] == 'MAS': count+=1
     # diag up right
+    if grid[y-1][x+1] == 'M' and grid[y-2][x+2] == 'A' and grid[y-3][x+3] == 'S': count+=1
     # diag up left
+    if grid[y-1][x-1] == 'M' and grid[y-2][x-2] == 'A' and grid[y-3][x-3] == 'S': count += 1
     # diag down right
+    if grid[y+1][x+1] == 'M' and grid[y-2][x+2] == 'A' and grid[y-3][x+3] == 'S': count += 1
     # diag down left
-
-    # check index to see which conditions can be checked
-    # inner frame
+    if grid[y+1][x-1] == 'M' and grid[y+2][x-2] == 'A' and grid[y+3][x-3] == 'S': count += 1
