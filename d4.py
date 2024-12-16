ae971abb995fd146315818c69dea6da479fdb70b
eric_cho <eric3cho@gmail.com>
2024-12-16 15:30:20 -0800

    fix list slicing

--- d4.py
+++ d4.py
@@ -5,8 +5,7 @@ import numpy as np
 def ceres_search():
     found = 0
     file = open('day4.txt')
-    lines = [line.strip() for line in file.readlines()]
-    grid = np.array(lines)
+    grid = [line.strip() for line in file.readlines()]
     for y in range(len(grid[0])):
         for x in range(y):
             if grid[y][x] == 'X':
@@ -14,18 +13,18 @@ def ceres_search():
     print(found)
 
 
-# given grid and coords of X, check around
+# given grid and coord of X, check around
 def check(grid, y, x):
     count = 0
     # RIGHT
-    if x < len(grid[0])-2:  # at least 7
-        if grid[y, x+1:x+4] == 'MAS': count+=1
+    if x < len(grid[0])-2:
+        if grid[y][x+1] == 'M' and grid[y][x+2] == 'A' and grid[y][x+3] == 'S': count+=1
     # LEFT
-    if x > 2:   # at least 3
-        if grid[y, x-3:x] == 'SAM': count+=1
+    if x > 2:
+        if grid[y][x-3] == 'S' and grid[y][x-2] == 'A' and grid[y][x-1] == 'M': count += 1
     # TOP
     if y > 2:
-        if grid[y-3:y, x] == 'SAM': count+=1
+        if grid[y-3][x] == 'S' and grid[y-2][x] == 'A' and grid[y-1][x] == 'M': count += 1
         # DIAG UP RIGHT
         if x < len(grid[0])-2:
             if grid[y - 1][x + 1] == 'M' and grid[y - 2][x + 2] == 'A' and grid[y - 3][x + 3] == 'S': count += 1
@@ -34,7 +33,7 @@ def check(grid, y, x):
             if grid[y - 1][x - 1] == 'M' and grid[y - 2][x - 2] == 'A' and grid[y - 3][x - 3] == 'S': count += 1
     # DOWN
     if y < len(grid)-2:
-        if grid[y+1:y+4, x] == 'MAS': count+=1
+        if grid[y+1][x] == 'M' and grid[y+2][x] == 'A' and grid[y+3][x] == 'S': count += 1
         # DIAG DOWN RIGHT
         if x < len(grid[0]) - 2:
             if grid[y + 1][x + 1] == 'M' and grid[y - 2][x + 2] == 'A' and grid[y - 3][x + 3] == 'S': count += 1
